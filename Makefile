4c57a8192d21df89fd44ea6583d962552f15baef
niforru <niforru@tutanota.com>
2023-09-07 19:03:47 +1000

    Just fixing some small stuff

--- Makefile
+++ Makefile
@@ -5,7 +5,6 @@ include config.mk
 
 SRC = drw.c dwm.c util.c
 OBJ = ${SRC:.c=.o}
-FILE=/etc/resolv.conf
 
 all: options dwm
 
--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Checks if pacman is installed and if so install dependencies and if not gives a helpful suggestion
-FILE=/etc/resolv.conf
+FILE=/bin/pacman
 if [ -f "$FILE" ]; then
     echo "Installing dependencies"; 
     sudo pacman -S libx11 libxft xorg-server xorg-xinit --noconfirm --needed;
