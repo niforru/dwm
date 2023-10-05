353940dfe1147fcab151a85b3103c28fc367c6f9
niforru <niforru@tutanota.com>
2023-10-05 14:17:44 +1100

    Future features working on it

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,8 +2,10 @@
 static const Block blocks[] = {
 	/*Icon*/		/*Command*/													/*Update Interval*/	/*Update Signal*/
 	{"Mem:", 		"free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",		30,					0},
+	/*{"Up",		"upspeed"													1					0},*/
+	/*{"Down",		"downspeed"														1,					0},*/
 	{"", 			"date '+%a %b %d %I:%M%p'",										5,					0},
-	{"Bat%: ", 	"cat /sys/class/power_supply/BAT0/capacity && echo %",		5,					0},
+	{"Bat%: ", 	"cat /sys/class/power_supply/BAT0/capacity",						5,					0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
