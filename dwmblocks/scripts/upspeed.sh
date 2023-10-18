1d45c5a907858ae6790a36d9884448592146d509
niforru <niforru@tutanota.com>
2023-10-18 23:24:35 +1100

    starting upspeed

--- dwmblocks/scripts/upspeed.sh
+++ dwmblocks/scripts/upspeed.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-default_route=$(ip route | grep default)
+interface="wlan0"  # Change this to your desired network interface
 
-interface=$(echo "$default_route" | awk '{print $5}')
-bytes_sent=$(cat /sys/class/net/$interface/statistics/tx_bytes)
-echo "Bytes Sent: $bytes_sent"
+# Check if the specified network interface exists
+if [ ! -d "/sys/class/net/$interface" ]; then
+    echo "Network interface $interface does not exist."
+    exit 1
+fi
+
+calculate_bytes_transmitted() {
+    interface="$1"
+    tx_bytes=$(ss -tin | awk -v iface="$interface" '$4 == iface && $1 == "ESTAB" {sum+=$2} END {print sum}')
+
+    tx_megabytes=$(echo "scale=2; $tx_bytes / 1048576" | bc)
+
+    echo "Uploaded: $tx_megabytes MB"
+}
+
+calculate_bytes_transmitted "$interface"
